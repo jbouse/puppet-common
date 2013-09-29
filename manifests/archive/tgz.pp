59147153d506fc3f94dec053321ead186c58634e
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2013-09-28 22:57:18 -0400

    Cleanup archive definitions
    Renamed tar-gz to tgz to clear up deprecation notices
    Syntax and formating cleanup of both definitions

---
+++ manifests/archive/tgz.pp
@@ -0,0 +1,17 @@
+define common::archive::tgz (
+  $ensure = present,
+  $source = undef,
+  $target = "/tmp/${name}"
+) {
+  validate_string($source)
+
+  Exec { path => '/usr/bin:/bin:/usr/sbin:/sbin' }
+
+  if $ensure != 'absent' {
+    exec { "${name} unpack":
+      command => "curl -s \"${source}\" | tar -xzf - -C ${target} --no-same-owner && touch ${name}",
+      creates => $name,
+      require => Package['curl'];
+    }
+  }
+}
--- manifests/archive/zip.pp
+++ manifests/archive/zip.pp
@@ -1,19 +1,17 @@
 define common::archive::zip (
-    $source,
-    $filename = basename($source),
-    $target
-){
+  $source,
+  $target,
+  $filename = basename($source),
+) {
+  exec { "${filename} download":
+    command => "curl -o /tmp/${filename} ${source}",
+    creates => "/tmp/${filename}",
+    require => Package['curl'],
+  }
 
-    exec {
-        "${filename} download":
-            command  => "curl -o /tmp/${filename} ${source}",
-            creates  => "/tmp/${filename}",
-            require => Package["curl"];
-
-        "${name} unpack":
-            command => "unzip /tmp/${filename} -d ${target} ; touch ${name}",
-            creates => $name,
-            require => Exec["${filename} download"];
-    }
+  exec { "${name} unpack":
+    command => "unzip /tmp/${filename} -d ${target} ; touch ${name}",
+    creates => $name,
+    require => Exec["${filename} download"];
+  }
 }
-
