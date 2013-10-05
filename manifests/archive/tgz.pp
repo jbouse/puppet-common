1b103d7b65a1dac91597de84e25daed02850dd38
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2013-10-05 04:02:48 -0400

    Add check for curl package and include resource if not present

--- manifests/archive/tgz.pp
+++ manifests/archive/tgz.pp
@@ -7,6 +7,10 @@ define common::archive::tgz (
 
   Exec { path => '/usr/bin:/bin:/usr/sbin:/sbin' }
 
+  if !defined(Package['curl']) {
+    package { 'curl': ensure => present }
+  }
+
   if $ensure != 'absent' {
     exec { "${name} unpack":
       command => "curl -s \"${source}\" | tar -xzf - -C ${target} --no-same-owner && touch ${name}",
