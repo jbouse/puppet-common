2c51902ab1757e5b7659e93c68685eecda73c4d8
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2014-02-02 16:54:03 -0500

    Remove apt::pin config and clean-up code

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,8 @@
 class common {
-  apt::pin { 'mcollective':
-    priority   => 700,
-    originator => 'Debian',
-    packages   => 'mcollective*',
-    codename   => $::lsbdistcodename,
-  }
-
   package { 'curl': ensure => 'present' }
 
-  mcollective::plugin { 'puppet': package => true }
+  mcollective::plugin { 'puppet':  package => true }
   mcollective::plugin { 'package': package => true }
   mcollective::plugin { 'service': package => true }
-  mcollective::plugin { 'nrpe': package => true }
+  mcollective::plugin { 'nrpe':    package => true }
 }
